b6201e828193a640a58b09f32438cef4031dbe6f
va <viktor.belebezyev@yandex.ru>
2023-02-17 17:46:09 +0300

    change solution

--- HW2/src/Bookcase/Case.java
+++ HW2/src/Bookcase/Case.java
@@ -5,17 +5,18 @@ import java.util.ArrayList;
 
 public class Case {
     // создадим массив для хранения объектов класса Book
-    static ArrayList<Book> caseBook = new ArrayList<>(Book.getBookList());
+//    static ArrayList<Book> caseBook = new ArrayList<>(Book.getBookList());
+
 
     public static int countBook(){
-        return caseBook.size();
+        return Book.getBookList().size();
     }
 
     public static void deleteBook(String s) {
-        if (caseBook.size() == 0) {
+        if (Book.getBookList().size() == 0) {
             System.out.println("Книг нет, в нашей библиотеке...");
         } else {
-            caseBook.removeIf(book -> (book.getNameBook().equalsIgnoreCase(s)) || (book.getWriterBook().equalsIgnoreCase(s)));
+            Book.getBookList().removeIf(book -> (book.getNameBook().equalsIgnoreCase(s)) || (book.getWriterBook().equalsIgnoreCase(s)));
         }
     }
 
@@ -26,14 +27,14 @@ public class Case {
         // проверим первый символ, если это цифра, то выведим всё по совпадающему году, если нет, то по автору или названию.
         if (Character.isDigit(s.charAt(0))) {
             int year = Integer.parseInt(s);
-            for (Book book: caseBook) {
+            for (Book book: Book.getBookList()) {
                 if (year == book.getDatePublication()) {
                     count++;
                     System.out.println(book);
                 }
             }
         } else if (Character.isLetter(s.charAt(2))){
-            for (Book book: caseBook) {
+            for (Book book: Book.getBookList()) {
                 if ((book.getNameBook().toLowerCase().contains(s))||(book.getWriterBook().toLowerCase().contains(s))) {
                     count++;
                     System.out.println(book);
@@ -47,7 +48,9 @@ public class Case {
 
     public static void addBook(String s) {
         String[] string = s.split(" ");
+        for (int i = 0; i < Book.getBookList().size(); i++) {
 
+        }
     }
 
 
